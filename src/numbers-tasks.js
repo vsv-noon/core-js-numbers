69ff93adf8f8e3eaf7a4e6f6807ca0babfee7985
ViacheslavAnisimov <forvsn@gmail.com>
2023-11-09 11:15:56 +0700

    feat: solved new tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -433,8 +433,7 @@ function getNumberValue(number) {
  * '5'      => false
  */
 function isNumber(number) {
-  // return Number.isFinite(number);
-  return Number.isInteger(number);
+  return Number.isFinite(number);
 }
 
 /**
@@ -462,8 +461,8 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
@@ -480,8 +479,8 @@ function getFloatOnString(/* str */) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
